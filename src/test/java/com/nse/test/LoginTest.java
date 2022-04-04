8520116494b5c32bca3c3ff4cec3fd11be0631ca
snehal jagtap <snehaljagtap051@gmail.com>
2022-04-04 17:45:49 +0530

    Data Utils complete

--- src/test/java/com/nse/test/LoginTest.java
+++ src/test/java/com/nse/test/LoginTest.java
@@ -1,9 +1,41 @@
 package com.nse.test;
 
+import org.openqa.selenium.By;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class LoginTest {
-	@Test
-	public void validCredential()
+import com.nse.base.WebDriverWrapper;
+import com.nse.utilities.DataUtils;
+
+public class LoginTest extends WebDriverWrapper{
+	
+	
+
+	
+	@Test (dataProviderClass = DataUtils.class,dataProvider = "invalidCredentialData")
+	public void invalidCredentialTest(String username, String password, String expectedTitle)
+	{
+		driver.findElement(By.id("txtUsername")).sendKeys(username);
+		driver.findElement(By.id("txtPassword")).sendKeys(password);
+		driver.findElement(By.id("btnLogin")).click();
+
+		String actualError = driver.findElement(By.id("spanMessage")).getText();
+		
+		Assert.assertEquals(actualError, expectedTitle);
+	}
+	
+	
+	@Test (dataProviderClass = DataUtils.class,dataProvider = "validCredentialData")
+	public void validCredentialTest(String username, String password, String expectedUrl)
+	{
+		driver.findElement(By.id("txtUsername")).sendKeys(username);
+		driver.findElement(By.id("txtPassword")).sendKeys(password);
+		driver.findElement(By.id("btnLogin")).click();
+
+		Assert.assertEquals(driver.getCurrentUrl(), expectedUrl);
+		
+
+	}
+
 
 }
